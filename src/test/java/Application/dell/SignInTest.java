99fd6f34f6444e910fdf07742042b682c5072a60
Mritunjoy Biswas <monjoy2005@yahoo.com>
2022-06-11 02:55:39 -0400

    friday late night push

--- src/test/java/Application/dell/SignInTest.java
+++ src/test/java/Application/dell/SignInTest.java
@@ -3,40 +3,40 @@ package Application.dell;
 import base.CommonAPI;
 import org.testng.Assert;
 import org.testng.annotations.Test;
-import pages.dell_Pages.CreateAccountPage;
-import pages.dell_Pages.DellFinancialSesvicesPage;
+import pages.dell_Pages.Dropdown_SignIn_CreateAccountPage;
+import pages.dell_Pages.Dropdown_SignIn_DellFinancialSesvicesPage;
 import pages.dell_Pages.HomePage;
-import pages.dell_Pages.SignInPage;
+import pages.dell_Pages.Dropdown_SignIn_SignInPage;
 
 public class SignInTest extends CommonAPI {
     @Test
     public void createAccountBtn(){
     HomePage homePage=new HomePage(getDriver());
-    CreateAccountPage createAccountPage =new CreateAccountPage(getDriver());
+    Dropdown_SignIn_CreateAccountPage dropdownSignInCreateAccountPage =new Dropdown_SignIn_CreateAccountPage(getDriver());
     homePage.hoverOverOnSignInBtn(getDriver());         //this is the format off calling hoverOver
     //hoverOver(getDriver(),homePage.signInHoverOver);        //this is NOT the format
     homePage.chickCreateAccountBtn();
-    isPresent(createAccountPage.createAccountText);
+    isPresent(dropdownSignInCreateAccountPage.createAccountText);
     }
 
     @Test
     public void signInBtn(){
     HomePage homePage=new HomePage(getDriver());
-    SignInPage signInPage=new SignInPage(getDriver());
-    CreateAccountPage createAccountPage =new CreateAccountPage(getDriver());
+    Dropdown_SignIn_SignInPage dropdownSignInSignInPage =new Dropdown_SignIn_SignInPage(getDriver());
+    Dropdown_SignIn_CreateAccountPage dropdownSignInCreateAccountPage =new Dropdown_SignIn_CreateAccountPage(getDriver());
     homePage.hoverOverOnSignInBtn(getDriver());
     homePage.clickSignInBtn();
     String expectedHeaderText = "Sign In";
-    Assert.assertEquals(expectedHeaderText,signInPage.signInPageHeaderText());
+    Assert.assertEquals(expectedHeaderText, dropdownSignInSignInPage.signInPageHeaderText());
     }
 
     @Test
     public void dellFinancialServiceBtn(){
     HomePage homePage=new HomePage(getDriver());
-    DellFinancialSesvicesPage dellFinancialSesvicesPage =new DellFinancialSesvicesPage(getDriver());
+    Dropdown_SignIn_DellFinancialSesvicesPage dropdownSignInDellFinancialSesvicesPage =new Dropdown_SignIn_DellFinancialSesvicesPage(getDriver());
     homePage.hoverOverOnSignInBtn(getDriver());
     homePage.clickFinancialServiceBtn();
-    isPresent(dellFinancialSesvicesPage.dellFinancialServiceText);
+    isPresent(dropdownSignInDellFinancialSesvicesPage.dellFinancialServiceText);
 
     }
 
